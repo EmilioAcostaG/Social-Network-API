29292e6c2bbd8eb1d65d3ff51d853ebe4fedb3e5
EmilioAcostaG <103851807+EmilioAcostaG@users.noreply.github.com>
2022-11-15 00:22:47 -0500

    repo setup, file structure started

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const { connect, connection } = require('mongoose');
+
+// Connection string to local instance of MongoDB including database name
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// Run npm install mongodb and require mongodb and MongoClient class
+// const mongodb = require('mongodb').MongoClient;
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
